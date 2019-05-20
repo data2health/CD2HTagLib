d231ee1a0334b2f1102e8bb5e266f2e45c3052f3
eichmann <david-eichmann@uiowa.edu>
2019-05-20 17:36:46 +0000

    Added LoE info to internal version of report.

--- src/main/java/org/cd2h/rppr/Generator.java
+++ src/main/java/org/cd2h/rppr/Generator.java
@@ -65,15 +65,18 @@ public class Generator extends BodyTagSupport {
 
     static Pattern projectPattern = Pattern.compile("^\\* \\[([^\\]]+)\\]\\(([^ \\)]+)\\)( +\\(\\[([^\\]]+)\\]\\(([^ \\)]+)\\)\\))?$");
     static Pattern imagePattern = Pattern.compile("^ *!\\[([^\\]]*)\\]\\(([^\\)]+)\\)(.*)$");
-    static List<List<Object>> sheet = null;
+    static List<List<Object>> budgetSheet = null;
+    static List<List<Object>> fteSheet = null;
 
     public static void main(String[] args) throws IOException, GeneralSecurityException, ParseException, SQLException, ClassNotFoundException {
 	PropertyConfigurator.configure(args[0]);
 	initialize();
 	showBudget = true; // presumably running on command line to generate docx
 	
-	if (showBudget)
+	if (showBudget) {
 	    loadBudgetSheet();
+	    loadFTESheet();
+	}
 	System.out.print(generate("admin"));
 	System.out.print(generate("informatics-maturity"));
 	System.out.print(generate("next-gen-data-sharing"));
@@ -135,71 +138,71 @@ public class Generator extends BodyTagSupport {
 		.setApplicationName(APPLICATION_NAME)
 		.build();
 	
-	sheet = service.spreadsheets().values().get(spreadsheetId, range).execute().getValues();
-	logger.debug("response: " + sheet);
+	budgetSheet = service.spreadsheets().values().get(spreadsheetId, range).execute().getValues();
+	logger.debug("response: " + budgetSheet);
     }
     
     static int projectBudget(String label) throws ParseException {
 	String stringValue = null;
 	switch (label) {
 	case "engagement":
-	    stringValue = (String) sheet.get(3).get(12);
+	    stringValue = (String) budgetSheet.get(3).get(12);
 	    break;
 	case "Operational-architecture":
-	    stringValue = (String) sheet.get(4).get(12);
+	    stringValue = (String) budgetSheet.get(4).get(12);
 	    break;
 	case "idea-challenge":
-	    stringValue = (String) sheet.get(5).get(12);
+	    stringValue = (String) budgetSheet.get(5).get(12);
 	    break;
 	case "governance-pathways":
-	    stringValue = (String) sheet.get(6).get(12);
+	    stringValue = (String) budgetSheet.get(6).get(12);
 	    break;
 	case "rdp-portal":
-	    stringValue = (String) sheet.get(8).get(12);
+	    stringValue = (String) budgetSheet.get(8).get(12);
 	    break;
 	case "maturity-model":
-	    stringValue = (String) sheet.get(7).get(12);
+	    stringValue = (String) budgetSheet.get(7).get(12);
 	    break;
 	case "data-harmonization":
-	    stringValue = (String) sheet.get(9).get(12);
+	    stringValue = (String) budgetSheet.get(9).get(12);
 	    break;
 	case "ehr2HPO.prj":
-	    stringValue = (String) sheet.get(11).get(12);
+	    stringValue = (String) budgetSheet.get(11).get(12);
 	    break;
 	case "hot-fhir-projects":
-	    stringValue = (String) sheet.get(10).get(12);
+	    stringValue = (String) budgetSheet.get(10).get(12);
 	    break;
 	case "architecting_attribution":
-	    stringValue = (String) sheet.get(12).get(12);
+	    stringValue = (String) budgetSheet.get(12).get(12);
 	    break;
 	case "edu-harmonization":
-	    stringValue = (String) sheet.get(13).get(12);
+	    stringValue = (String) budgetSheet.get(13).get(12);
 	    break;
 	case "information-architecture":
-	    stringValue = (String) sheet.get(14).get(12);
+	    stringValue = (String) budgetSheet.get(14).get(12);
 	    break;
 	case "menRva":
-	    stringValue = (String) sheet.get(15).get(12);
+	    stringValue = (String) budgetSheet.get(15).get(12);
 	    break;
 	case "CTS-Personas":
 	    stringValue = "0"; //(String) sheet.get(15).get(12);
 	    break;
 	case "scits-platform":
-	    stringValue = (String) sheet.get(16).get(12);
+	    stringValue = (String) budgetSheet.get(16).get(12);
 	    break;
 	case "Cloud-Tool-Architecture":
 	case "Cloud-Tool-Archtecture":
-	    stringValue = (String) sheet.get(17).get(12);
+	    stringValue = (String) budgetSheet.get(17).get(12);
 	    break;
 	case "leaf-edw":
-	    stringValue = (String) sheet.get(18).get(12);
+	    stringValue = (String) budgetSheet.get(18).get(12);
 	    break;
 	case "DREAM-Challenge":
 	case "mortality-prediction":
-	    stringValue = (String) sheet.get(19).get(12);
+	    stringValue = (String) budgetSheet.get(19).get(12);
 	    break;
 	case "competitions-project":
-	    stringValue = (String) sheet.get(20).get(12);
+	    stringValue = (String) budgetSheet.get(20).get(12);
 	    break;
 	default:
 	    System.err.println("unmatched project name: " + label);
@@ -225,6 +228,87 @@ public class Generator extends BodyTagSupport {
 	    System.err.println("unmatched core name: " + label);
 	    return -1;
 	}
+    }
+
+    static void loadFTESheet() throws GeneralSecurityException, IOException, ParseException {
+	final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+	final String spreadsheetId = google_prop_file.getProperty("fte.spreadsheetId");
+	final String range = "FTExProj!A1:B25";
+	Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, GoogleAPI.getCredentials(HTTP_TRANSPORT, SCOPES, google_prop_file.getProperty("sheets.credentials"), google_prop_file.getProperty("sheets.tokens")))
+		.setApplicationName(APPLICATION_NAME)
+		.build();
+	
+	fteSheet = service.spreadsheets().values().get(spreadsheetId, range).execute().getValues();
+	logger.debug("response: " + fteSheet);
+    }
+    
+    static String projectFTE(String label) throws ParseException {
+	String stringValue = null;
+	switch (label) {
+	case "engagement":
+	    stringValue = (String) fteSheet.get(1).get(1);
+	    break;
+	case "Operational-architecture":
+	    stringValue = (String) fteSheet.get(2).get(1);
+	    break;
+	case "idea-challenge":
+	    stringValue = (String) fteSheet.get(3).get(1);
+	    break;
+	case "governance-pathways":
+	    stringValue = (String) fteSheet.get(5).get(1);
+	    break;
+	case "rdp-portal":
+	    stringValue = (String) fteSheet.get(6).get(1);
+	    break;
+	case "maturity-model":
+	    stringValue = (String) fteSheet.get(4).get(1);
+	    break;
+	case "data-harmonization":
+	    stringValue = (String) fteSheet.get(7).get(1);
+	    break;
+	case "ehr2HPO.prj":
+	    stringValue = (String) fteSheet.get(9).get(1);
+	    break;
+	case "hot-fhir-projects":
+	    stringValue = (String) fteSheet.get(8).get(1);
+	    break;
+	case "architecting_attribution":
+	    stringValue = (String) fteSheet.get(11).get(1);
+	    break;
+	case "edu-harmonization":
+	    stringValue = (String) fteSheet.get(14).get(1);
+	    break;
+	case "information-architecture":
+	    stringValue = (String) fteSheet.get(13).get(1);
+	    break;
+	case "menRva":
+	    stringValue = (String) fteSheet.get(12).get(1);
+	    break;
+	case "CTS-Personas":
+	    stringValue = "0"; //(String) sheet.get(14).get(1);
+	    break;
+	case "scits-platform":
+	    stringValue = (String) fteSheet.get(10).get(1);
+	    break;
+	case "Cloud-Tool-Architecture":
+	case "Cloud-Tool-Archtecture":
+	    stringValue = (String) fteSheet.get(18).get(1);
+	    break;
+	case "leaf-edw":
+	    stringValue = (String) fteSheet.get(15).get(1);
+	    break;
+	case "DREAM-Challenge":
+	case "mortality-prediction":
+	    stringValue = (String) fteSheet.get(17).get(1);
+	    break;
+	case "competitions-project":
+	    stringValue = (String) fteSheet.get(16).get(1);
+	    break;
+	default:
+	    System.err.println("unmatched project name: " + label);
+	    stringValue =  "";
+	}
+	return stringValue;
     }
 
     public static String generate(String repoName) throws IOException, ParseException, SQLException {
@@ -336,6 +420,24 @@ public class Generator extends BodyTagSupport {
 	    buffer.append("suppressed until final release\n");
     }
 
+    static void generateProjectFTE(String project, StringBuffer buffer) throws ParseException, IOException {
+	if (!showBudget)
+	    return;
+	
+	buffer.append("##### CD2H Personnel Level of Effort\n");
+	String buf = null;
+	BufferedReader reader = new BufferedReader(new StringReader(projectFTE(project)));
+	reader.readLine(); // discard redundant header
+	reader.readLine(); // discard redundant header
+	while ((buf = reader.readLine()) != null) {
+	    buf = buf.trim(); // don't need the indentation
+	    if (buf.startsWith("#"))
+		buffer.append("###" + buf + "\n");
+	    else
+		buffer.append("* " + buf + "\n");
+	}
+    }
+
     static void generateProject(String reference, StringBuffer buffer) throws IOException, ParseException {
 	Matcher matcher = projectPattern.matcher(reference);
 	if (matcher.matches()) {
@@ -382,6 +484,7 @@ public class Generator extends BodyTagSupport {
 			case "# Budget":
 			    projectMode = Modes.BUDGET;
 			    generateProjectBudget(url.substring(url.lastIndexOf("/")+1),buffer);
+			    generateProjectFTE(repo_name, buffer);
 			    break;
 			}
 			break;
